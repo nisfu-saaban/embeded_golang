cca73be951393b640a7e132ebf496a6bdab60f35
nisfu-saaban <nisthy07@gmail.com>
2023-06-05 06:14:20 +0700

    embeded file golang

---
+++ embed_test.go
@@ -0,0 +1,14 @@
+package embededgolang
+
+import (
+	_ "embed"
+	"fmt"
+	"testing"
+)
+
+//go:embed version.txt
+var version string
+
+func TestEmbed(t *testing.T) {
+	fmt.Println(version)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/nisfu-saaban/embeded_golang
+
+go 1.19
